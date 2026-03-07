ce70e710010a3d194cd54546e6dbe0e24a0a275b
Ursula113355 <Ursula113355@gmail.com>
2026-03-07 11:21:55 +0800

    Fix the bug of unable to read the file_path

--- csv_grades_analyse.py
+++ csv_grades_analyse.py
@@ -1,6 +1,5 @@
 import pandas as pd
-file_path = 'student_scores.csv'
+file_path = r'D:\ruixi\Aria\rw\职业规划\python练习\python_data_learning\student_scores.csv'
 df = pd.read_csv(file_path)
-stats = df.describe().loc[['mean','max','min']]
+stats = df.describe().loc[['mean', 'max', 'min']]
 print(stats)
-
